b258e450bc5f17cc913661d821b299cf17ceee3c
maria.uliniuc <maria.uliniuc@student.upt.ro>
2023-07-12 20:47:37 +0300

    Am adaugat a doua tema acestui program.

--- src/main/java/readSpark/Main.java
+++ src/main/java/readSpark/Main.java
@@ -1,15 +1,19 @@
 package readSpark;
 
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class Main {
     public static void main(String csv[]){
+        //prima tema
         SparkSession sparkSession= SparkSession.builder().master("local").appName("Read_CSV").getOrCreate();
         Dataset<Row> dataset = sparkSession.read().option("header","true").csv("C:/Users/Ulniuc Maria/Desktop/javaPractice/learnSpark/src/main/resources/erasmus.csv");
-
-
-        dataset.show(15, false);
+        //dataset.show(25, false);
+        //a doua tema
+        dataset.printSchema();
+        dataset.select("Receiving Country Code","Sending Country Code").show(20,false);
+        dataset.groupBy("Receiving Country Code","Sending Country Code").count().withColumnRenamed("count","Number of students").show();
     }
 }
